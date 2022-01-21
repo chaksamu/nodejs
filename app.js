a827bd78ed9195c4199cbe370e896d0b8fc61ac4
chaksamu <45867824+chaksamu@users.noreply.github.com>
2022-01-21 14:01:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello,World!!!")
+var a = 7
+var b = 2
+var c = a+b
+
+console.log("The Sum of Two Numbers is : " +c)
+console.log("Hello World")
+
+
+var calc = require('./calc.js')
+
+result = calc.add(4,5)
+
+console.log("The Sum of Two Numbers is : " + result)
+
+result = calc.sub(5,3)
+
+console.log("The Sub of Two Numbers is : " + result)
